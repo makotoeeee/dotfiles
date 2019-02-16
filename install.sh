14d0be9a65a1074d0ec8539b41c353e272b95825
makotoeeee <mkttoyoda@gmail.com>
2019-02-16 21:31:21 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
 echo "[INFO] Starting......"
+
+readonly DOTFILES_DIR := ~/.ghq/github.com/makotoeeee/dotfiles
+readonly REPO_URL := https://github.com/makotoeeee/dotfiles
+
+if [ ! -d $DOTFILES_DIR ]; then
+  git clone $REPO_URL $DOTFILES_DIR
+fi
+
+cd $DOTFILES_DIR
 make all
+
 echo "[info] Finished!"
