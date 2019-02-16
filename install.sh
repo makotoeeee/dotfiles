89de7df44f37f9567a5a8c7da8dcb1c6e38fc297
makotoeeee <mkttoyoda@gmail.com>
2019-02-16 21:25:05 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,22 +1,5 @@
 #!/bin/bash
 
 echo "[INFO] Starting......"
-
-readonly DIR=~/.ghq/github.com/makotoeeee
-
-if [ ! -d ${DIR} ]; then
-  mkdir -p ${DIR}
-fi
-
-cd ${DIR}
-
-readonly DOTFILES_DIR=~/.ghq/github.com/makotoeeee/dotfiles
-
-if [ ! -n "$(ls $DOTFILES_DIR)" ]; then
-  git clone https://github.com/makotoeeee/dotfiles
-fi
-
-cd ${DOTFILES_DIR}
 make all
-
 echo "[info] Finished!"
