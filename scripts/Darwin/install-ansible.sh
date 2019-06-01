5f32677e1607c2aba3a781121e86196e582d9d4d
makotoeeee <mkttoyoda@gmail.com>
2019-06-01 22:55:36 +0900

    refactoring scripts/Darwin/install-ansible.sh

--- scripts/Darwin/install-ansible.sh
+++ scripts/Darwin/install-ansible.sh
@@ -2,13 +2,19 @@
 path=$(cd $(dirname $0); pwd)
 source $path/../utils/log.sh
 
-ilog "Start installing ansible"
+install_ansible() {
+  if test ! $(which ansible); then
+    brew install ansible
+    ilog "Finish installing ansible"
+    exit 0
+  fi
+}
 
-if test ! $(which ansible); then
-  brew install ansible
-  ilog "Finish installing ansible"
+main() {
+  ilog "Start installing ansible"
+  install_ansible
+  ilog "ansible is already installed. Skip installing ansible"
   exit 0
-fi
+}
 
-ilog "ansible is already installed. Skip installing ansible"
-exit 0
+main
