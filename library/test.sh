b9d207c32a4f2acfff1c6ee463aa19eec5a2f299
makotoeeee <mkttoyoda@gmail.com>
2019-07-06 10:53:15 +0900

    Update library/test.sh

--- library/test.sh
+++ library/test.sh
@@ -2,6 +2,6 @@
 
 touch hoge 
 ls -l hoge*
-ansible localhost -i hosts --connection=local -m move -a "src=hoge" --module-path=.
+ansible localhost -i ../hosts --connection=local -m move -a "src=hoge" --module-path=.
 ls -l hoge*
 rm hoge*
