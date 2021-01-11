18e0d52e88458877ef582cfaa49a2bddf86b2e97
makotoeeee <mkttoyoda@gmail.com>
2021-01-11 14:43:15 +0900

    Remove the scripts directory to use setup.sh

--- scripts/Darwin/uninstall-homebrew.sh
+++
@@ -1,3 +0,0 @@
-if test $(which brew); then
-    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/uninstall)"
-fi
--- scripts/utils/log.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-function ilog {
-  echo "$(date  +'%FT%T') [info] $1"
-}
-
-function elog {
-  echo "$(date  +'%FT%T') [error] $1"
-}
